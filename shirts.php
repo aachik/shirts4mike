9dc0d725f14d95276e4053606675a992b34aef9c
aachik <abdulhamid@outlook.com>
2015-02-28 19:49:36 -0600

    Initial commit

---
+++ shirts.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = 'Full catalog of Mike&rsquo;s shirts';
+include('inc/header.php'); ?>
+
+    <div class="section page">
+
+        <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+
+    </div>
+
+<?php include('inc/footer.php'); ?>
